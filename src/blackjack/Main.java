34170dd9d67255919b53c5bf7dc0870ce24c19b2
kerim <yoatomadd@gmail.com>
2022-12-25 21:28:47 +0100

    cosmetics

--- src/blackjack/Main.java
+++ src/blackjack/Main.java
@@ -23,7 +23,7 @@ public class Main {
             do {
                 System.out.println(
                         Design.RED
-                                +"\n-------------------------------------------" +
+                                + "\n-------------------------------------------" +
                                 "\n🃏🃏🎰 Classic and Simple Black Jack 🎰🃏🃏\n" +
                                 "-------------------------------------------\n");
                 System.out.println(Design.RESET + "(1) - Play");
@@ -46,18 +46,13 @@ public class Main {
                             System.out.println("\n(min: " + Game.MIN_BET + " | all-in: " + player.getMoney() + ")");
                             System.out.print("Your bet: ");
                             bet = sc.nextInt();
-
                             //Check if bet is set properly (withing the proper range)
                             while (bet > player.getMoney() || bet < Game.MIN_BET) {
-                                System.out.print(Design.RED + "Attention!! You can bet only between "
-                                        + Game.MIN_BET + " and "
-                                        + player.getMoney() + ": "
-                                        + Design.RESET);
+                                System.out.print(Design.RED + "Attention!! You can bet only between " + Game.MIN_BET +
+                                        " and " + player.getMoney() + ": " + Design.RESET);
                                 bet = sc.nextInt();
                             }
-
                             player.setBet(bet);
-
                             bjack = new Game(player, stats);
                             bjack.run();
                         } else
@@ -65,36 +60,28 @@ public class Main {
                                     + "\n You don't have enough money to play. Top-up your account! (option 2)"
                                     + Design.RESET);
                         break;
-
                     case 2:
                         if (player.getMoney() < Game.MIN_BET) {
                             player.resetMoney();
                             System.out.println(Design.BLUE
-                                    + "\nYour balance successfully renewed and now you have PLN "
-                                    + player.getDefaultMoney() + "."
-                                    + Design.RESET);
+                                    + "\nYour balance successfully renewed and now you have PLN " +
+                                    player.getDefaultMoney() + Design.RESET);
                         } else
-                            System.out.println(Design.RED
-                                    + "\nAttention!! You have enough money to continue playing"
+                            System.out.println(Design.RED + "\nAttention!! You have enough money to continue playing"
                                     + Design.RESET);
                         break;
-
                     case 3:
-                        System.out
-                                .println(Design.YELLOW + "\n>>> Your balance: PLN " + player.getMoney() + " <<<"
-                                        + Design.RESET);
+                        System.out.println(Design.YELLOW + "\n>>> Your balance: PLN " + player.getMoney() + " <<<"
+                                + Design.RESET);
                         break;
                     case 4:
-                        File file = new File(
-                                "src/blackjack/Help.txt");
+                        File file = new File("src/blackjack/Help.txt");
                         Scanner txt_sc = new Scanner(file);
                         while (txt_sc.hasNextLine()) {
                             System.out.println(txt_sc.nextLine());
                             Thread.sleep(400);
-
                         }
                         break;
-
                     case 5:
                         if (!name_entered) {
                             System.out.print("\nEnter your name (Default: 'Unknown player'): ");
@@ -109,11 +96,8 @@ public class Main {
                     case 0:
                         break;
                     default:
-                        System.out.println(
-                                Design.RED_BACKGROUND + "\nWrong option: " + option
-                                        + Design.RESET);
+                        System.out.println(Design.RED_BACKGROUND + "\nWrong option: " + option + Design.RESET);
                         break;
-
                 }
             } while (option != 0);
         } catch (InputMismatchException ignored) {
